70d5b1d6cbadd49666a58c0f8c8b05a1d765ed04
Joel Daniel Perez Camey <dperez@yetis.io>
2020-11-07 08:12:08 -0600

    update

--- Torneos/views.py
+++ Torneos/views.py
@@ -51,10 +51,7 @@ class TorneoView(APIView):
             index_jugador = random.randrange(0,len(jugadores))
             jugador2 = jugadores.pop(index_jugador)
             partida = Partida.objects.create(jugador1=jugador1, jugador2=jugador2, torneo=torneo, orden=i)
-            params = {
-                "id": os.environ['ID_TOKEN'],
-                "secret": os.environ['SECRET_TOKEN']
-            }
+            params = "?id="+os.environ['ID_TOKEN']+"&secret="+os.environ['SECRET_TOKEN']            
             dataRequests  = requests.get(os.environ['URL_TOKEN'], params = params)
             data = json.loads(dataRequests)
             myobj = {
